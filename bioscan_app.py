0cb9b38ca88dcdc0cc9c8957293c68247fccf604
c-a-ptacek <cody.ptacek@ku.edu>
2022-10-08 20:26:51 -0500

    bioscan_app: added disclaimers & confirmations, fixed wording

--- bioscan_app.py
+++ bioscan_app.py
@@ -2,7 +2,7 @@
 Author(s):
     Cody Ptacek
 Date Created: 10/04/2022
-Last Modified: 10/04/2022
+Last Modified: 10/08/2022
 Purpose:
     The web app file for BioScan. Uses streamlit. 
     Application takes an image from user and classifies the category of skin condition that 
@@ -16,7 +16,7 @@ import streamlit as st
 import numpy as np
 import tensorflow as tf
 import cv2
-from PIL import Image, ImageOps
+from PIL import Image
 
 #=========================Importing the model=================================
 model = tf.keras.models.load_model('fixed_last_layer.hdf5')
@@ -34,14 +34,10 @@ def image_conversion(image):
     Purpose:
         Processes a provided image so that it can be input into a model.
     """
-    size = (150,150)
-
-    #image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
     image = np.array(image)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #Converts image to RGB. Better for model.
     image = cv2.resize(image, IMAGE_SIZE) #Resizes the image to a standard size for the model.
-    #image = np.array(image, dtype= 'float32') #Honestly, not sure what this does or if it is needed...
 
     st.write(image.shape)
 
@@ -61,10 +57,18 @@ def run_model(image, model):
     st.write(conversion.shape)
     return model.predict(conversion)
 
+def edit_prediction(prediction):
+    prediction[0] = 'Benign'
+    prediction[1] = 'Malignant'
+
 #================================Webpage Code====================================
 #Text to display on the webpage.
-st.write('# BioScan')
-st.write('An image classifier.\n\n')
+st.title('BioScan')
+st.subheader('An image classifier.\n\n')
+st.sidebar.subheader('Disclaimer')
+st.sidebar.write('This is a program created by four computer science students who are not capable of giving valid medical advaice.')
+st.sidebar.write('Please do not use this site as valid medical advice. ')
+st.header('Instructions')
 st.write('To use BioScan take a picture of your skin condition and save it as a jpg or png.')
 st.write('Import the image below.')
 
@@ -77,16 +81,35 @@ if user_image is None:
     st.text("Please upload an image file!")
 else:
     #If image is provided
-    image = Image.open(user_image) #opens the image-like element uploades as an image
-    st.image(image, use_column_width=True) #displays the provided image on the web app
-    prediction = run_model(image, model) #runs the image through the model stores as in variable prediction
+    if st.button("Acknowledge Disclaimer"):
+        image = Image.open(user_image) #opens the image-like element uploades as an image
+        st.image(image, use_column_width=True) #displays the provided image on the web app
+        prediction = run_model(image, model) #runs the image through the model stores as in variable prediction
 
-    #Gives answer based on prediction.
-    #I guessed how the output would look. We should discuss together.
-    if np.argmax(prediction) == 0:
-        st.write("It is likely benign")
+        #Gives answer based on prediction.
+        #I guessed how the output would look. We should discuss together.
+        if np.argmax(prediction) == 0:
+            st.write("Our image clasifier labeled it as 'Benign'")
+            st.subheader("Remember:")
+            st.write("This is an image classifier created by four students.")
+            st.write("It should not be taken as valid medical advice.")
+            st.write("If you are concerned about your condition, see a doctor!")
+        else:
+            st.header("Results:")
+            st.write("Our image classifier labeled it as 'Malignant'")
+            st.header("Remember:")
+            st.write("This is an image classifier created by four students.")
+            st.write("It should not be taken as valid medical advice.")
+            st.write("If you are concerned about your condition, see a doctor!")
+        #Shold present the prediction as a nice little table of % likelihoods. 
+        st.write(prediction)
+
+        #edit_prediction(prediction)
+
+        #st.write(prediction)
+
     else:
-        st.write("It is likely malignant! See a doctor!")
-
-    #Shold present the prediction as a nice little table of % likelihoods. 
-    st.write(prediction)
+        st.header('Acknowledge Disclaimer')
+        st.write('Before your results are processed, please acknowledge that you understand that this program does not provide valid medical advice.')
+        st.write("It is a program writen by four students and should not be taken as medical advice.")
+        st.write("If you are concerned about your health, visit a doctor.")
