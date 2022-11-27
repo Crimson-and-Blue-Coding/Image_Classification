d9707bc242c9e1142cfcbcc2d3c5bc3f3ce06d6d
c-a-ptacek <cody.ptacek@ku.edu>
2022-11-26 19:35:32 -0600

    implementing smaller_model.h5
    Made changes to implement smaller_model.h5

--- bioscan_app.py
+++ bioscan_app.py
@@ -22,7 +22,7 @@ import pandas as pd
 from conditions import Conditions
 
 #=========================Importing the model=================================
-model = tf.keras.models.load_model('efficientnet.hdf5')
+model = tf.keras.models.load_model('smaller_model.h5')
 
 #=============================Constant Variables==================================
 IMAGE_SIZE = (150, 150)
