689acd877c5b11ae8c43fd765619fd8c9c7521d6
c-a-ptacek <cody.ptacek@ku.edu>
2022-11-26 19:43:49 -0600

    attempting to implement model.json 2
    Helps if you double check for typos... Hopefully this works!

--- bioscan_app.py
+++ bioscan_app.py
@@ -22,7 +22,7 @@ import pandas as pd
 from conditions import Conditions
 
 #=========================Importing the model=================================
-model = tf.keras.model_from_json('model.json')
+model = tf.keras.models.model_from_json('model.json')
 
 #=============================Constant Variables==================================
 IMAGE_SIZE = (150, 150)
